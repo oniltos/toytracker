8fc343678bac6bf6ac1c6b8f26405d0d0e0c91ca
Nilton Freitas <nilton.freitas@ironhack.com>
2022-12-06 22:04:13 -0300

    Adiciona navbar e homepage

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">ToyTracker</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
